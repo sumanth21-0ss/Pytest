231e480de8092daa4c694b91252ddf416af9a50d
sumanth <sumanthking21@gmail.com>
2022-09-06 13:09:57 +0530

    Normal Test

--- main.py
+++ main.py
@@ -11,5 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print(f'Hi, {"th"}')  # Press Ctrl+F8 to toggle the breakpoint.
  
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
